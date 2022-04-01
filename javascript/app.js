021380db2825bdd8e74fe0985a6374bab40a20b5
Simeon <simeon@MacBook-Pro.local>
2022-04-01 12:24:12 +0100

    project setup

---
+++ javascript/app.js
@@ -0,0 +1,50 @@
+console.log("hello world");
+
+//array
+
+var details = [
+    "onions",
+    "Abakaliki",
+    12,
+    90,
+    true,
+    false,
+    ["vegies", "tables", "mice"],
+];
+
+//objects
+
+const details = {
+    names: "John && Sally",
+    age: 29,
+    dob: "01-20-1994",
+};
+
+const isLarger = 10 < 2;
+
+// if (isLarger) {
+//     console.log("10 is greater");
+// } else {
+//     console.log("Not fulfilled");
+// }
+
+
+if (10 < 2) {
+        console.log("10 is greater");
+    } else if (12 > 5) {
+        console.log("5 is lesser");
+    } else if (5 == 5) {
+        console.log("5 equals 5")
+    } else {
+        console.log("No condition fulfilled")
+    }
+
+
+//LOOP
+
+let start = 0;
+let end = 10;
+
+for (start; start < end; start++) {
+    
+}
---
+++ javascript/arithmetics.js
@@ -0,0 +1,43 @@
+// var age1 = 30;
+// var age2 = 25;
+
+// var totalage = age1 + age2;
+
+// console.log
+
+// var names = "James Doe";
+// var age = "25";
+// var address = "Yaba, Lagos";
+
+// console.log(
+//     "Hello Everyone, I am " +
+//     names +
+//     ". I live at " +
+//     address +
+//     ". I am " +
+//     age +
+//     "years old."
+// )
+
+// console.log(
+//     `Hello Everyone, My name is ${names}. I am ${age} years old.`
+// )
+
+// var admin = "John";
+// var name = "John";
+
+// alert (admin)
+
+// Conditonal statements
+// if (10 > 2) {
+//     console.log("10 is greater");
+// } else if (12 < 5) {
+//     console.log("5 is lesser");
+// } else if (5 == 5) {
+//     console.log("5 equals 5")
+// } else {
+//     console.log("No condition fulfilled")
+// }
+
+
+
